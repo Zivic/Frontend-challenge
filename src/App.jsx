50e6252f406c82808b5bbbdc6c3f36a98af9dfd3
Zivic <djzivic@gmail.com>
2023-07-10 17:22:49 +0200

    fix: typo

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <div className=" flex h-screen w-full">
     <img className=' box-border w-7/12 h-full object-fit'
     src="../src/assets/background_dragon.png" 
-    alt="jedigovna"
+    alt="background_dragon.png"
     />
     <Sidebar/>
     </div>

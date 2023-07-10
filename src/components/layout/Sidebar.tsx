4983a8a15084c5cfb392d1c19adee27ea0794a19
Zivic <djzivic@gmail.com>
2023-07-10 17:17:40 +0200

    feat: add image and sidebar Remove boilerplate Add custom colors for gradient to tailwind config

---
+++ src/components/layout/Sidebar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+const Sidebar = () => {
+  return <>
+    <div className="box-border m-0  w-5/12 h-full bg-gradient-to-r from-sidebar-gradient-start to-sidebar-gradient-end">
+
+    </div>
+  </>;
+};
+export default Sidebar;

38457a9540fab45df1a2cd59c4f643364020db3b
Zivic <djzivic@gmail.com>
2023-07-13 18:51:56 +0200

    style: add card border gradient, change colors
    note: border-image does not work with border-radius
    
    Change colors to the correct values

--- src/components/layout/NewsCard.tsx
+++ src/components/layout/NewsCard.tsx
@@ -4,10 +4,14 @@ const NewsCard = (props) => {
     console.log(props)
     return (
         <>
-        <div className=" bg-gradient-to-r from-transparent to-gray-900/75 rounded-3xl mt-10">
+        <div className="rounded-3xl mt-10 custom-border">
+
+        <div className=" bg-gradient-to-r from-transparent to-card-gradient-end rounded-3xl">
             <img src="/src/assets/book.svg" alt="book.svg"  className=" "/>
             {props.children}
         </div>
+        </div>
+
         </>
     )
 }

04d64328038711829c57eff779d7d0b2b64b5c2a
Zivic <djzivic@gmail.com>
2023-07-16 14:47:02 +0200

    style: add timeline background gradient stripes

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -8,6 +8,11 @@ const Sidebar = () => {
     ">
       <h1 className=" mt-20 text-white text-3xl">Updates</h1>
       <p className=" mt-10 text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur sed ut tenetur, voluptates minima facilis veniam libero suscipit facere optio sint corrupti iure harum aliquam quasi aspernatur illo! Aliquid, enim.</p>
+      <div className="">
+      <div className="absolute w-1 h-2/3 lines">
+      </div>
+
+      <div className="">
       <NewsCard>
         <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel quia rem minima animi impedit ex suscipit sunt ab necessitatibus fugit dolore tenetur, deleniti est perspiciatis libero iste ducimus ipsum molestias!</p>
       </NewsCard>
@@ -17,6 +22,8 @@ const Sidebar = () => {
       <NewsCard>
         <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel quia rem minima animi impedit ex suscipit sunt ab necessitatibus fugit dolore tenetur, deleniti est perspiciatis libero iste ducimus ipsum molestias!</p>
       </NewsCard>
+      </div>
+     </div>
     </div>
   </>;
 };

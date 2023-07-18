27696ca22e8d726bceb9120b9ef1100a537f685b
Zivic <djzivic@gmail.com>
2023-07-18 21:03:02 +0200

    style: sidebar and navbar gradient borders
    lesson: shapes created with clip-path are incompatible with borders

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
     return(
         <>
         <div className="flex left-point absolute right-0 z-20 w-7/12 h-16  bg-gradient-to-r from-sidebar-gradient-start to-sidebar-gradient-end
-         justify-end px-10 gap-10 text-white
+         justify-end px-10 gap-10 text-white custom-border-3
         ">
             <button>Home</button>
             <button>Browse</button>

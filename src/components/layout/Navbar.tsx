f3881c7bf6dccbdd7e0c2a907ca1c7833862222e
Zivic <djzivic@gmail.com>
2023-07-22 19:17:39 +0200

    feat: custom hook for responsive layout

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,9 +1,13 @@
 import React from "react";
-
+import { useState } from "react";
+import useViewport from "../../hooks/useViewport";
 const Navbar = () => {
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+  const {width, isSmallScreen} = useViewport();
+  console.log("Small screen: ", isSmallScreen);
   return (
     <>
-      <div
+      <nav
         className="flex left-point absolute right-0 z-20 w-7/12 h-16  bg-gradient-to-r from-sidebar-gradient-start to-sidebar-gradient-end
          justify-end px-10 gap-16 text-white custom-border-navbar
         "
@@ -13,7 +17,7 @@ const Navbar = () => {
         <button>Updates</button>
         <button>Pricing</button>
         <button className="custom-button rounded-lg px-10 my-4">Sign up</button>
-      </div>
+      </nav>
       <div className="absolute left-0 flex flex-row p-10 pl-20 gap-4">
         <img src="/src/assets/Logo.png" alt="Logo.png"></img>
         <img src="/src/assets/Serendipity.svg" alt="Serendipity.svg"></img>

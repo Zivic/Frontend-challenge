1be0d4ca278107c1370950ad6c87621482440618
Zivic <djzivic@gmail.com>
2023-07-24 17:37:58 +0200

    style: custom button to hide sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -13,19 +13,18 @@ function App() {
         <div className=" box-border w-full md:w-9/12 lg:w-10/12 h-full object-cover object-center dragon custom-bg-size bg-no-repeat">
           {isSmallScreen && !isSidebarOpen && (
             <button
-              className="relative top-1/2 left-1/3 z-30 custom-button"
+              className="relative top-1/2 left-1/3 z-30 custom-button rounded-xl p-4 text-white font-medium"
               onClick={() => setIsSidebarOpen(!isSidebarOpen)}
             >
-              OPEN SIDEBAR
+              See updates
             </button>
           )}
           {isSmallScreen && isSidebarOpen && (
-            <button
-              className="fixed top-1/2 left-0 z-30 custom-button"
+            <div
+              className="fixed top-1/2 z-30 w-24 h-24 -left-12 hover:cursor-pointer close-button"
               onClick={() => setIsSidebarOpen(!isSidebarOpen)}
             >
-              Close SIDEBAR
-            </button>
+            </div>
           )}
         </div>
         <Navbar />
